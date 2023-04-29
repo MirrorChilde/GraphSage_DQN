3e956f775f56d996e8677e5e5ebe625879754038
MirrorChilde <2940297105@qq.com>
2023-04-29 21:00:11 +0800

    修改图神经网络

---
+++ GAT/networks.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+import pandas as pd
+import os
+import warnings
+
+class GraphAttention(layers.Layer):
+    def __init__(
+        self,
+        units,
+        kernel_initializer="glorot_uniform",
+        kernel_regularizer=None,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+        self.units = units
+        self.kernel_initializer = keras.initializers.get(kernel_initializer)
+        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
+
+    def build(self, input_shape):
+
+        self.kernel = self.add_weight(
+            shape=(input_shape[0][-1], self.units),
+            trainable=True,
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            name="kernel",
+        )
+        self.kernel_attention = self.add_weight(
+            shape=(self.units * 2, 1),
+            trainable=True,
+            initializer=self.kernel_initializer,
+            regularizer=self.kernel_regularizer,
+            name="kernel_attention",
+        )
+        self.built = True
+
+    def call(self, inputs):
+        states_action, edges = inputs
+
+        # 线性变换节点状态
+        node_states_transformed = tf.matmul(states_action, self.kernel)
+
+        # (1) 计算成对的注意力得分
+        node_states_expanded = tf.gather(node_states_transformed, edges) # 从node_states_transformed的axis=0维根据edges的参数值获取切片
+        node_states_expanded = tf.reshape(
+            node_states_expanded, (tf.shape(edges)[0], -1)
+        )
+        attention_scores = tf.nn.leaky_relu(
+            tf.matmul(node_states_expanded, self.kernel_attention)
+        )
+        attention_scores = tf.squeeze(attention_scores, -1)
+
+        # (2) Normalize attention scores
+        attention_scores = tf.math.exp(tf.clip_by_value(attention_scores, -2, 2))
+        attention_scores_sum = tf.math.unsorted_segment_sum(
+            data=attention_scores,
+            segment_ids=edges[:, 0],
+            num_segments=tf.reduce_max(edges[:, 0]) + 1,
+        )
+        attention_scores_sum = tf.repeat(
+            attention_scores_sum, tf.math.bincount(tf.cast(edges[:, 0], "int32"))
+        )
+        attention_scores_norm = attention_scores / attention_scores_sum
+
+        # (3) 收集邻居节点状态，应用注意评分并聚合
+        node_states_neighbors = tf.gather(node_states_transformed, edges[:, 1]) # 从node_states_transformed的axis=0维根据edges[ : 1]的参数值获取切片
+
+        out = tf.math.unsorted_segment_sum(
+            data=node_states_neighbors * attention_scores_norm[:, tf.newaxis],
+            segment_ids=edges[:, 0],
+            num_segments=tf.shape(states_action)[0],
+        )
+
+        return out
+
+
+class MultiHeadGraphAttention(layers.Layer):
+    def __init__(self, units, num_heads=8, merge_type="concat", **kwargs):
+        super().__init__(**kwargs)
+        self.num_heads = num_heads
+        self.merge_type = merge_type
+        self.attention_layers = [GraphAttention(units) for _ in range(num_heads)]
+
+    def call(self, inputs):
+        atom_features, pair_indices = inputs
+
+        # 从每个注意力头获得输出
+        outputs = [
+            attention_layer([atom_features, pair_indices])
+            for attention_layer in self.attention_layers
+        ]
+        # 连接或平均每个头的节点状态
+        if self.merge_type == "concat":
+            outputs = tf.concat(outputs, axis=-1)
+        else:
+            outputs = tf.reduce_mean(tf.stack(outputs, axis=-1), axis=-1)
+        # 激活并返回节点状态
+        return tf.nn.relu(outputs)
